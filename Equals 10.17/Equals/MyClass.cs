71da991f1efa97ec87ea1b9cc4ad69797be832d3
smcilvenny <s.mcilvenny@students.clark.edu>
2019-10-17 14:43:55 -0700

    Thurs Lab Demos

---
+++ Equals 10.17/Equals/MyClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equals
+{
+    class MyClass
+    {
+        private int someNumber;
+        private string someString;
+
+        public MyClass (int num, string str)
+        {
+            this.someNumber = num;
+            this.someString = str;
+        }
+
+        public override bool Equals(object obj)
+        {
+          
+
+            if(obj is MyClass && obj is null)
+            {
+                //return base.Equals(obj);
+                //compare
+                //temp variable
+                MyClass temp;
+                temp = (MyClass)obj;
+
+                if((temp.someNumber == this.someNumber) && 
+                   temp.someString.Equals(this.someString)) return true;
+                else return false;
+            }
+            return false;
+        }
+        
+            
+        
+    }
+}
